3ec4f27eb172bccd4e9da2811eee7a2136db69ec
Peter Spada <peterspa@microsoft.com>
2020-07-21 20:06:50 -0700

    SetPanelChildFocusAction

--- retron.designer/DesignerApp/Placeholders.cs
+++ retron.designer/DesignerApp/Placeholders.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Interactivity;
 
 namespace ff
 {
@@ -14,5 +16,13 @@ namespace ff
     {
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) => new NotImplementedException();
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => new NotImplementedException();
+    }
+
+    public class SetPanelChildFocusAction : TargetedTriggerAction<UIElement>
+    {
+        protected override void Invoke(object parameter)
+        {
+            _ = this.Target?.Focus();
+        }
     }
 }
