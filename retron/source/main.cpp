88deac0cb3d3aa380b67674aa5c14fc93fea9614
Peter Spada <spadapet@hotmail.com>
2024-08-07 09:50:32 -0700

    Use combined resources in a content file

--- retron/source/main.cpp
+++ retron/source/main.cpp
@@ -5,19 +5,6 @@
 #include "source/ui/particle_lab_page.xaml.h"
 #include "source/ui/title_page.xaml.h"
 
-namespace res
-{
-    void register_bonus();
-    void register_controls();
-    void register_electrode();
-    void register_game_spec();
-    void register_graphics();
-    void register_particles();
-    void register_player();
-    void register_sprites();
-    void register_xaml();
-}
-
 static std::shared_ptr<ff::game::app_state_base> create_app_state()
 {
     return std::make_shared<retron::app_state>();
@@ -25,15 +12,7 @@ static std::shared_ptr<ff::game::app_state_base> create_app_state()
 
 static void register_global_resources()
 {
-    ::res::register_bonus();
-    ::res::register_controls();
-    ::res::register_electrode();
-    ::res::register_game_spec();
-    ::res::register_graphics();
-    ::res::register_particles();
-    ::res::register_player();
-    ::res::register_sprites();
-    ::res::register_xaml();
+    verify(ff::global_resources::add_files(ff::filesystem::executable_path().parent_path()));
 
     Noesis::RegisterComponent<Noesis::EnumConverter<retron::game_flags>>();
     Noesis::RegisterComponent<Noesis::EnumConverter<retron::game_players>>();
